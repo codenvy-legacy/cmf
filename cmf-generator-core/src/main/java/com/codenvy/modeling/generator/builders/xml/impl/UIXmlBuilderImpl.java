a334f4101c1bb2256b402409e8b140dc07774900
Andrey Plotnikov <aplotnikov@codenvy.com>
2014-04-18 18:53:33 +0300

    CMF-1: Added implementation of ui xml builder and tests

---
+++ cmf-generator-core/src/main/java/com/codenvy/modeling/generator/builders/xml/impl/UIXmlBuilderImpl.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2014 Codenvy, S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codenvy.modeling.generator.builders.xml.impl;
+
+import com.codenvy.modeling.generator.builders.xml.api.GField;
+import com.codenvy.modeling.generator.builders.xml.api.GStyle;
+import com.codenvy.modeling.generator.builders.xml.api.UIXmlBuilder;
+import com.codenvy.modeling.generator.builders.xml.api.widgets.GWidget;
+import com.google.inject.Inject;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Andrey Plotnikov
+ */
+public class UIXmlBuilderImpl implements UIXmlBuilder {
+
+    private static final String HEADER       = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    private static final String XMLNS_FORMAT = "\n             xmlns:%s='%s'";
+
+    @Nonnull
+    private List<GField>        fields;
+    @Nonnull
+    private Map<String, String> xmlns;
+    @Nullable
+    private GStyle              style;
+    @Nullable
+    private GWidget             widget;
+
+    @Inject
+    public UIXmlBuilderImpl() {
+        clean();
+    }
+
+    /** Clean builder configuration */
+    private void clean() {
+        style = null;
+        widget = null;
+        fields = new ArrayList<>();
+
+        xmlns = new LinkedHashMap<>();
+        xmlns.put("ui", "urn:ui:com.google.gwt.uibinder");
+    }
+
+    /** {@inheritDoc} */
+    @Nonnull
+    @Override
+    public UIXmlBuilder withXmlns(@Nonnull String prefix, @Nonnull String importPath) {
+        xmlns.put(prefix, importPath);
+        return this;
+    }
+
+    /** {@inheritDoc} */
+    @Nonnull
+    @Override
+    public UIXmlBuilder withField(@Nonnull GField field) {
+        fields.add(field);
+        return this;
+    }
+
+    /** {@inheritDoc} */
+    @Nonnull
+    @Override
+    public UIXmlBuilder withStyle(@Nonnull GStyle style) {
+        this.style = style;
+        return this;
+    }
+
+    /** {@inheritDoc} */
+    @Nonnull
+    @Override
+    public UIXmlBuilder setWidget(@Nonnull GWidget widget) {
+        this.widget = widget;
+        return this;
+    }
+
+    /** {@inheritDoc} */
+    @Nonnull
+    @Override
+    public String build() throws IllegalStateException {
+        StringBuilder xml = new StringBuilder(HEADER);
+
+        xml.append("<ui:UiBinder ");
+        for (Map.Entry<String, String> xmln : xmlns.entrySet()) {
+            xml.append(String.format(XMLNS_FORMAT, xmln.getKey(), xmln.getValue()));
+        }
+        xml.append(">\n");
+
+        for (GField field : fields) {
+            xml.append(field.build()).append('\n');
+        }
+
+        if (style != null) {
+            xml.append(style.build()).append('\n');
+        }
+
+        if (widget != null) {
+            xml.append(widget.build()).append('\n');
+        }
+
+        xml.append("</ui:UiBinder>");
+
+        clean();
+
+        return xml.toString();
+    }
+
+}
---
+++ cmf-generator-core/src/test/java/com/codenvy/modeling/generator/UIXmlBuilderImplTest.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2014 Codenvy, S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codenvy.modeling.generator;
+
+import com.codenvy.modeling.generator.builders.xml.api.GField;
+import com.codenvy.modeling.generator.builders.xml.api.GStyle;
+import com.codenvy.modeling.generator.builders.xml.api.UIXmlBuilder;
+import com.codenvy.modeling.generator.builders.xml.api.widgets.GWidget;
+import com.codenvy.modeling.generator.builders.xml.impl.UIXmlBuilderImpl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Here we're testing {@link UIXmlBuilderImpl}.
+ *
+ * @author Andrey Plotnikov
+ */
+public class UIXmlBuilderImplTest {
+
+    private UIXmlBuilder builder;
+
+    @Before
+    public void setUp() throws Exception {
+        builder = new UIXmlBuilderImpl();
+    }
+
+    @Test
+    public void simpleXmlShouldBeCreated() throws Exception {
+        String actualXML = builder.build();
+
+        String expectedXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                             "<ui:UiBinder \n" +
+                             "             xmlns:ui='urn:ui:com.google.gwt.uibinder'>\n" +
+                             "</ui:UiBinder>";
+
+        assertEquals(expectedXML, actualXML);
+    }
+
+    @Test
+    public void xmlWithFewXmlnsShouldBeCreated() throws Exception {
+        String actualXML = builder.withXmlns("p", "urn:import:com.google.gwt.user.client.ui")
+                                  .withXmlns("p1", "urn:import:com.google.gwt.user.cellview.client")
+                                  .build();
+
+        String expectedXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                             "<ui:UiBinder \n" +
+                             "             xmlns:ui='urn:ui:com.google.gwt.uibinder'\n" +
+                             "             xmlns:p='urn:import:com.google.gwt.user.client.ui'\n" +
+                             "             xmlns:p1='urn:import:com.google.gwt.user.cellview.client'>\n" +
+                             "</ui:UiBinder>";
+
+        assertEquals(expectedXML, actualXML);
+    }
+
+    @Test
+    public void xmlWithFewFieldsShouldBeCreated() throws Exception {
+        String content1 = "field 1";
+        String content2 = "field 2";
+
+        GField field1 = createField(content1);
+        GField field2 = createField(content2);
+
+        String actualXML = builder.withField(field1)
+                                  .withField(field2)
+                                  .build();
+
+        String expectedXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                             "<ui:UiBinder \n" +
+                             "             xmlns:ui='urn:ui:com.google.gwt.uibinder'>\n" +
+                             content1 + '\n' +
+                             content2 + '\n' +
+                             "</ui:UiBinder>";
+
+        assertEquals(expectedXML, actualXML);
+
+        verify(field1).build();
+        verify(field2).build();
+    }
+
+    private GField createField(String content) {
+        GField field = Mockito.mock(GField.class);
+        when(field.build()).thenReturn(content);
+
+        return field;
+    }
+
+    @Test
+    public void xmlWithWidgetShouldBeCreate() throws Exception {
+        String content = "widget";
+        GWidget widget = createWidget(content);
+
+        String actualXML = builder.setWidget(widget).build();
+
+        String expectedXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                             "<ui:UiBinder \n" +
+                             "             xmlns:ui='urn:ui:com.google.gwt.uibinder'>\n" +
+                             content + '\n' +
+                             "</ui:UiBinder>";
+
+        assertEquals(expectedXML, actualXML);
+
+        verify(widget).build();
+    }
+
+    private GWidget createWidget(String content) {
+        GWidget widget = mock(GWidget.class);
+        when(widget.build()).thenReturn(content);
+
+        return widget;
+    }
+
+    @Test
+    public void xmlWithStyleShouldBeCreate() throws Exception {
+        String content = "style";
+        GStyle style = createStyle(content);
+
+        String actualXML = builder.withStyle(style).build();
+
+        String expectedXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                             "<ui:UiBinder \n" +
+                             "             xmlns:ui='urn:ui:com.google.gwt.uibinder'>\n" +
+                             content + '\n' +
+                             "</ui:UiBinder>";
+
+        assertEquals(expectedXML, actualXML);
+
+        verify(style).build();
+    }
+
+    private GStyle createStyle(String content) {
+        GStyle style = mock(GStyle.class);
+        when(style.build()).thenReturn(content);
+
+        return style;
+    }
+
+    @Test
+    public void complexXMLShouldBeCreated() throws Exception {
+        String fieldContent = "field";
+        String styleContent = "style";
+        String widgetContent = "widget";
+
+        GField field = createField(fieldContent);
+        GStyle style = createStyle(styleContent);
+        GWidget widget = createWidget(widgetContent);
+
+        String actualXML = builder.withField(field)
+                                  .withStyle(style)
+                                  .setWidget(widget)
+                                  .build();
+
+        String expectedXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                             "<ui:UiBinder \n" +
+                             "             xmlns:ui='urn:ui:com.google.gwt.uibinder'>\n" +
+                             fieldContent + '\n' +
+                             styleContent + '\n' +
+                             widgetContent + '\n' +
+                             "</ui:UiBinder>";
+
+        assertEquals(expectedXML, actualXML);
+
+        verify(field).build();
+        verify(style).build();
+        verify(widget).build();
+    }
+
+}
