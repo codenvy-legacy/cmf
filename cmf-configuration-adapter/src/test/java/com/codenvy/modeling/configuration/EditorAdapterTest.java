665de87b9ee0308e8e1afcdeb260901e0e6c96b2
Valeriy Svydenko <vsvydenko@codenvy.com>
2014-04-25 14:36:00 +0300

    CMF-6: added some tests for testing antlr grammars

--- cmf-configuration-adapter/src/test/java/com/codenvy/modeling/configuration/EditorAdapterTest.java
+++ cmf-configuration-adapter/src/test/java/com/codenvy/modeling/configuration/EditorAdapterTest.java
@@ -18,22 +18,206 @@ package com.codenvy.modeling.configuration;
 
 import com.codenvy.modeling.adapter.editor.EditorConfigurationAdapter;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Dmitry Kuleshov
+ * @author Valeriy Svydenko
  */
 public class EditorAdapterTest {
 
-    public static final String EDITOR_GRAMMAR_TEST_I = "/EditorGrammarTest_I";
+    private static final String EDITOR_GRAMMAR_TEST_I     = "/EditorGrammarTest_I";
+    private static final String EDITOR_CONFIGURATION_IMPL = "EditorConfigurationImpl";
+    private static final String TOOLBAR_IMPL              = "ToolbarImpl";
+    private static final String SIZE_IMPL                 = "SizeImpl";
+    private static final String GROUP_IMPL                = "GroupImpl";
+    private static final String ITEM_IMPL                 = "ItemImpl";
+    private static final String TEXT_IMPL                 = "TextImpl";
+    private static final String PANEL_IMPL                = "PanelImpl";
+    private static final String WORKSPACE_IMPL            = "WorkspaceImpl";
 
-    @Test
-    public void testExampleField() throws Exception {
+    private String      editorConfiguration;
+    private String[]    configurationElements;
+    private Set<String> tbAlignment;
+    private Set<String> alignment;
+    private Set<String> scrollability;
+
+    @Before
+    public void setUp() throws Exception {
+        tbAlignment = new HashSet<>(Arrays.asList("SOUTH", "NORTH", "WEST", "EAST"));
+        alignment = new HashSet<>(Arrays.asList("TOP", "BOTTOM", "LEFT", "RIGHT", "CENTER"));
+        scrollability = new HashSet<>(Arrays.asList("YES", "NO"));
+
         String configurationFile = getClass().getResource(EDITOR_GRAMMAR_TEST_I).getFile();
         EditorConfigurationAdapter editorConfigurationAdapter = new EditorConfigurationAdapter(configurationFile);
+        editorConfiguration = editorConfigurationAdapter.getConfiguration().toString();
+        configurationElements = editorConfiguration.split("\n");
 
-        System.out.println("===============================================================================" + "\n\n");
-        System.out.println(editorConfigurationAdapter.getConfiguration().toString());
-        System.out.println("\n\n" + "===============================================================================");
+    }
+
+    @Test
+    public void editorConfigurationShouldBeContainedEditorConfigurationImplString() {
+        assertTrue(editorConfiguration.contains(EDITOR_CONFIGURATION_IMPL));
+    }
+
+    @Test
+    public void editorConfigurationShouldBeContainedToolbarImplString() {
+        assertTrue(editorConfiguration.contains(TOOLBAR_IMPL));
+    }
+
+    @Test
+    public void toolbarShouldBeContainedAlignmentParameter() {
+        String toolbarDescription = getDescriptionOfConfigurationElement(TOOLBAR_IMPL);
+
+        assertTrue(toolbarDescription.contains("alignment"));
+    }
+
+    @Test
+    public void valueOfAlignmentShouldBeDefined() {
+        String toolbarDescription = getDescriptionOfConfigurationElement(TOOLBAR_IMPL);
+
+        int indexOfAlign = toolbarDescription.indexOf("alignment");
+        toolbarDescription = toolbarDescription.substring(indexOfAlign);
+        indexOfAlign = toolbarDescription.indexOf('=');
+
+        assertTrue(tbAlignment.contains(toolbarDescription.substring(indexOfAlign + 1, toolbarDescription.indexOf(','))));
+    }
+
+    @Test
+    public void editorConfigurationShouldBeContainedSizeImplString() {
+        assertTrue(editorConfiguration.contains(SIZE_IMPL));
+    }
+
+    @Test
+    public void sizeShouldBeContainedTwoArguments() {
+        String sizeDescription = getDescriptionOfConfigurationElement(SIZE_IMPL);
+        int indexOfCompact = sizeDescription.indexOf("compact");
+        int indexOfFull = sizeDescription.indexOf("full");
+
+        String compactValue = sizeDescription.substring(indexOfCompact);
+        compactValue = compactValue.substring(compactValue.indexOf('=') + 1);
+        compactValue = compactValue.substring(0, compactValue.indexOf(','));
+
+        String fullValue = sizeDescription.substring(indexOfFull);
+        fullValue = fullValue.substring(fullValue.indexOf('=') + 1);
+        fullValue = fullValue.substring(0, fullValue.indexOf('}'));
+
+        assertTrue(indexOfCompact > -1);
+        assertTrue(indexOfFull > -1);
+
+        assertTrue(Integer.parseInt(compactValue) > 0);
+        assertTrue(Integer.parseInt(fullValue) > 0);
+    }
+
+    @Test
+    public void editorConfigurationShouldBeContainedGroupImplString() {
+        assertTrue(editorConfiguration.contains(GROUP_IMPL));
+    }
+
+    @Test
+    public void groupShouldBeContainedMargin() {
+        String groupDescription = getDescriptionOfConfigurationElement(GROUP_IMPL);
+
+        assertTrue(groupDescription.contains("margin"));
+    }
+
+    @Test
+    public void editorConfigurationShouldBeContainedItemImplString() {
+        assertTrue(editorConfiguration.contains(ITEM_IMPL));
+    }
+
+    @Test
+    public void itemShouldBeContainedSizeAndMarginParameters() {
+        String itemDescription = getDescriptionOfConfigurationElement(ITEM_IMPL);
+        int indexOfSize = itemDescription.indexOf("size");
+
+        String valueOfSize = itemDescription.substring(indexOfSize);
+        valueOfSize = valueOfSize.substring(valueOfSize.indexOf('=') + 1);
+        valueOfSize = valueOfSize.substring(0, valueOfSize.indexOf(','));
+
+        assertTrue(itemDescription.contains("size"));
+        assertTrue(itemDescription.contains("margin"));
+        assertTrue(Integer.parseInt(valueOfSize) > 0);
+    }
+
+    @Test
+    public void editorConfigurationShouldBeContainedTextImplString() {
+        assertTrue(editorConfiguration.contains(TEXT_IMPL));
+    }
+
+    @Test
+    public void textImplShouldBeContainedAlignment() {
+        String textDescription = getDescriptionOfConfigurationElement(TEXT_IMPL);
+        int indexOfAlignment = textDescription.indexOf("alignment");
+
+        String valueOfAlignment = textDescription.substring(indexOfAlignment);
+        valueOfAlignment = valueOfAlignment.substring(valueOfAlignment.indexOf("=") + 1);
+        valueOfAlignment = valueOfAlignment.substring(0, valueOfAlignment.indexOf('}'));
+
+        assertTrue(alignment.contains(valueOfAlignment));
+
+    }
+
+    @Test
+    public void editorConfigurationShouldBeContainedPanelImplString() {
+        assertTrue(editorConfiguration.contains(PANEL_IMPL));
+    }
+
+    @Test
+    public void panelShouldBeContainedAlignmentAndDefaultSizeParam() {
+        String panelDescription = getDescriptionOfConfigurationElement(PANEL_IMPL);
+        int indexOfAlignment = panelDescription.indexOf("alignment");
+        int indexOfSize = panelDescription.indexOf("defaultSize");
+
+        String valueOfAlignment = panelDescription.substring(indexOfAlignment);
+        valueOfAlignment = valueOfAlignment.substring(valueOfAlignment.indexOf('=') + 1);
+        valueOfAlignment = valueOfAlignment.substring(0, valueOfAlignment.indexOf(','));
+
+        String valueOfSize = panelDescription.substring(indexOfSize);
+        valueOfSize = valueOfSize.substring(valueOfSize.indexOf('=') + 1);
+        valueOfSize = valueOfSize.substring(0, valueOfSize.indexOf('}'));
+
+        assertTrue(indexOfAlignment > -1);
+        assertTrue(indexOfSize > -1);
+
+        assertTrue(new Integer(valueOfSize) > 0);
+        assertTrue(tbAlignment.contains(valueOfAlignment));
+
+    }
+
+    @Test
+    public void editorConfigurationShouldBeContainedWorkspaceImplString() {
+        assertTrue(editorConfiguration.contains(WORKSPACE_IMPL));
+    }
+
+    @Test
+    public void workspaceShouldBeContainedScrollabilityParameter() {
+        String workspaceDescription = getDescriptionOfConfigurationElement(WORKSPACE_IMPL);
+        int indexOfWorkspace = workspaceDescription.indexOf("scrollability");
+
+
+        String valueOfScrollability = workspaceDescription.substring(indexOfWorkspace);
+        valueOfScrollability = valueOfScrollability.substring(valueOfScrollability.indexOf('=') + 1);
+        valueOfScrollability = valueOfScrollability.substring(0, valueOfScrollability.indexOf("}"));
+
+        assertTrue(indexOfWorkspace > -1);
+        assertTrue(scrollability.contains(valueOfScrollability));
+
+    }
+
+    private String getDescriptionOfConfigurationElement(String element) {
+        for (String configurationElement : configurationElements) {
+            if (configurationElement.startsWith(element)) {
+                return configurationElement;
+            }
+        }
+        return "";
     }
 }
--- cmf-configuration-adapter/src/test/java/com/codenvy/modeling/configuration/metamodel/diagram/DiagramAdapterTest.java
+++ cmf-configuration-adapter/src/test/java/com/codenvy/modeling/configuration/metamodel/diagram/DiagramAdapterTest.java
@@ -18,22 +18,197 @@ package com.codenvy.modeling.configuration.metamodel.diagram;
 
 import com.codenvy.modeling.adapter.metamodel.diagram.DiagramConfigurationAdapter;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Dmitry Kuleshov
+ * @author Valeriy Svydenko
  */
 public class DiagramAdapterTest {
 
-    public static final String DIAGRAM_GRAMMAR_TEST_I = "/DiagramGrammarTest_I";
+    private static final String DIAGRAM_CONFIGURATION  = "DiagramConfigurationImpl";
+    private static final String ELEMENT                = "ElementImpl";
+    private static final String PROPERTY               = "PropertyImpl";
+    private static final String COMPONENT              = "ComponentImpl";
+    private static final String CONNECTION             = "ConnectionImpl";
+    private static final String DIAGRAM_GRAMMAR_TEST_I = "/DiagramGrammarTest_I";
+    private static final String CONNECTIONS            = "connections";
+    private static final String COMPONENTS             = "components";
+    private static final String PROPERTIES             = "properties";
 
-    @Test
-    public void testExampleField() throws Exception {
+    private String      diagramConfiguration;
+    private String[]    configurationElements;
+    private Set<String> connectionType;
+    private Set<String> connectionRelation;
+
+    @Before
+    public void setUp() throws Exception {
+        connectionType = new HashSet<>(Arrays.asList("DIRECTED", "NONDIRECTED", "POSITIONAL"));
+        connectionRelation = new HashSet<>(Arrays.asList("SINGLE", "MULTIPLE"));
+
         String configurationFile = getClass().getResource(DIAGRAM_GRAMMAR_TEST_I).getFile();
         DiagramConfigurationAdapter diagramConfigurationAdapter = new DiagramConfigurationAdapter(configurationFile);
+        diagramConfiguration = diagramConfigurationAdapter.getConfiguration().toString();
+        configurationElements = diagramConfiguration.split("\n");
+    }
 
-        System.out.println("===============================================================================" + "\n\n");
-        System.out.println(diagramConfigurationAdapter.getConfiguration().toString());
-        System.out.println("\n\n" + "===============================================================================");
+
+    @Test
+    public void diagramConfigurationShouldBeContainedDiagramConfigurationString() {
+        assertTrue(diagramConfiguration.contains(DIAGRAM_CONFIGURATION));
+    }
+
+    @Test
+    public void diagramConfigurationShouldBeContainedElementString() {
+        assertTrue(diagramConfiguration.contains(ELEMENT));
+        assertTrue(diagramConfiguration.contains("elements"));
+    }
+
+    @Test
+    public void elementShouldBeHadName() {
+        String element = getDescriptionOfConfigurationElement(ELEMENT);
+
+        int indexOfName = element.indexOf("name");
+        String nameValue = element.substring(indexOfName);
+        nameValue = nameValue.substring(nameValue.indexOf("=") + 2);
+        nameValue = nameValue.substring(0, nameValue.indexOf('\''));
+
+        assertTrue(indexOfName > -1);
+        assertTrue(nameValue.length() > 0);
+    }
+
+    @Test
+    public void diagramConfigurationShouldBeContainedPropertyString() {
+        assertTrue(diagramConfiguration.contains(PROPERTIES));
+    }
+
+    @Test
+    public void propertyShouldBeContainedThreeParameters() {
+        String propertyImpl = getDescriptionOfConfigurationElement(PROPERTY);
+
+        if (propertyImpl.length() > 0) {
+            int indexOfName = propertyImpl.indexOf("name");
+            String name = propertyImpl.substring(indexOfName);
+            name = name.substring(name.indexOf("=") + 2);
+            name = name.substring(0, name.indexOf('\''));
+
+            int indexOfValue = propertyImpl.indexOf("value");
+            String value = propertyImpl.substring(indexOfValue);
+            value = value.substring(value.indexOf("=") + 2);
+            value = value.substring(0, value.indexOf('\''));
+
+            int indexOfType = propertyImpl.indexOf("type");
+            String type = propertyImpl.substring(indexOfType);
+            type = type.substring(type.indexOf("=") + 1);
+            type = type.substring(0, type.indexOf('}'));
+
+            assertTrue(indexOfName > -1);
+            assertTrue(name.length() > 0);
+
+            assertTrue(indexOfValue > -1);
+            assertTrue(value.length() > 0);
+
+            assertTrue(indexOfType > -1);
+            assertTrue(type.length() > 0);
+        } else {
+            int indexOfProperties = diagramConfiguration.indexOf(PROPERTIES);
+            String propValue =
+                    diagramConfiguration.substring(indexOfProperties + PROPERTIES.length(), indexOfProperties + PROPERTIES.length() + 3);
+
+            assertEquals("=[]", propValue);
+        }
+    }
+
+    @Test
+    public void diagramConfigurationShouldBeContainedConnectionString() {
+        assertTrue(diagramConfiguration.contains(CONNECTIONS));
+    }
+
+    @Test
+    public void connectionShouldBeContainedForParameters() {
+        String connectionImpl = getDescriptionOfConfigurationElement(CONNECTION);
+
+        if (connectionImpl.length() > 0) {
+            int indexOfName = connectionImpl.indexOf("name");
+            String name = connectionImpl.substring(indexOfName);
+            name = name.substring(name.indexOf("=") + 2);
+            name = name.substring(0, name.indexOf('\''));
+
+            int indexOfDestination = connectionImpl.indexOf("destination");
+            String destination = connectionImpl.substring(indexOfDestination);
+            destination = destination.substring(destination.indexOf("=") + 2);
+            destination = destination.substring(0, destination.indexOf('\''));
+
+            int indexOfType = connectionImpl.indexOf("type");
+            String type = connectionImpl.substring(indexOfType);
+            type = type.substring(type.indexOf("=") + 1);
+            type = type.substring(0, type.indexOf('}'));
+
+            int indexOfRelation = connectionImpl.indexOf("relation");
+            String relation = connectionImpl.substring(indexOfRelation);
+            relation = relation.substring(relation.indexOf("=") + 1);
+            relation = relation.substring(0, relation.indexOf(','));
+
+            assertTrue(indexOfName > -1);
+            assertTrue(name.length() > 0);
+
+            assertTrue(indexOfDestination > -1);
+            assertTrue(destination.length() > 0);
+
+            assertTrue(indexOfType > -1);
+            assertTrue(connectionType.contains(type));
+
+            assertTrue(indexOfRelation > -1);
+            assertTrue(connectionRelation.contains(relation));
+        } else {
+            int indexOfProperties = diagramConfiguration.indexOf(CONNECTIONS);
+            String propValue =
+                    diagramConfiguration.substring(indexOfProperties + CONNECTIONS.length(), indexOfProperties + CONNECTIONS.length() + 3);
+
+            assertEquals("=[]", propValue);
+        }
+    }
+
+    @Test
+    public void diagramConfigurationShouldBeContainedComponentString() {
+        assertTrue(diagramConfiguration.contains(COMPONENTS));
+    }
+
+    @Test
+    public void componentShouldBeContainedOneParameters() {
+        String componentImpl = getDescriptionOfConfigurationElement(COMPONENT);
+
+        if (componentImpl.length() > 0) {
+            int indexOfName = componentImpl.indexOf("name");
+            String name = componentImpl.substring(indexOfName);
+            name = name.substring(name.indexOf("=") + 2);
+            name = name.substring(0, name.indexOf('\''));
+
+            assertTrue(indexOfName > -1);
+            assertTrue(name.length() > 0);
+        } else {
+            int indexOfProperties = diagramConfiguration.indexOf(COMPONENTS);
+            String propValue =
+                    diagramConfiguration.substring(indexOfProperties + COMPONENTS.length(), indexOfProperties + COMPONENTS.length() + 3);
+
+            assertEquals("=[]", propValue);
+        }
+    }
+
+    private String getDescriptionOfConfigurationElement(String element) {
+        for (String configurationElement : configurationElements) {
+            if (configurationElement.startsWith(element)) {
+                return configurationElement;
+            }
+        }
+        return "";
     }
 }
