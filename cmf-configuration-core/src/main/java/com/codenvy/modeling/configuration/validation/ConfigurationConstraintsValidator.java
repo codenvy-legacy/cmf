53888410223c66596826a5a5ef6025f2f95769b3
Dmitry Kuleshov <dkuleshov@codenvy.com>
2014-05-05 16:37:52 +0300

    CMF-9: added basic validator

---
+++ cmf-configuration-core/src/main/java/com/codenvy/modeling/configuration/validation/ConfigurationConstraintsValidator.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2014 Codenvy, S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codenvy.modeling.configuration.validation;
+
+import com.codenvy.modeling.configuration.Configuration;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+/**
+ * @author Dmitry Kuleshov
+ */
+public class ConfigurationConstraintsValidator {
+
+    public static Report validate(Object object){
+        Report report = Report.getEmptyReport();
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+        for (ConstraintViolation<?> violation : validator.validate(object)){
+            report.addError(violation.getMessage());
+        }
+
+        return report;
+    }
+}
---
+++ cmf-configuration-core/src/test/java/com/codenvy/modeling/configuration/validation/ConfigurationConstraintsValidatorTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2014 Codenvy, S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codenvy.modeling.configuration.validation;
+
+import com.codenvy.modeling.configuration.editor.Group;
+import com.codenvy.modeling.configuration.editor.Item;
+import com.codenvy.modeling.configuration.editor.Text;
+import com.codenvy.modeling.configuration.editor.Toolbar;
+
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Dmitry Kuleshov
+ */
+public class ConfigurationConstraintsValidatorTest {
+    @Test
+    public void simpleConstraintViolationShouldResultInError() {
+        Group group = new Group();
+        //breaking constraint
+        group.setMargin("");
+
+        Report report = ConfigurationConstraintsValidator.validate(group);
+
+        assertTrue(report.hasErrors());
+        assertEquals(1, report.getErrors().size());
+    }
+
+    @Test
+    public void innerFieldClassConstraintViolationShouldResultInError() {
+        Text text = new Text();
+        Item item = new Item();
+
+        //breaking constraint
+        text.setAlignment(null);
+
+        item.setMargin("Margin");
+        item.setAlignment(Item.Alignment.BOTTOM);
+        item.setSize(0);
+        item.setText(text);
+
+        Report report = ConfigurationConstraintsValidator.validate(item);
+
+        assertTrue(report.hasErrors());
+        assertEquals(1, report.getErrors().size());
+    }
+}
