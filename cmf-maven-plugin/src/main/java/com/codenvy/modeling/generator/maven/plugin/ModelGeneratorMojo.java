9bb3fc7ef555136c4f96c2b3d55e5cbfd8946069
Andrey Plotnikov <aplotnikov@codenvy.com>
2014-05-13 15:32:19 +0300

    CMF-1: Created generator utility

--- cmf-maven-plugin/src/main/java/com/codenvy/modeling/generator/maven/plugin/ModelGeneratorMojo.java
+++ cmf-maven-plugin/src/main/java/com/codenvy/modeling/generator/maven/plugin/ModelGeneratorMojo.java
@@ -1,9 +1,6 @@
 package com.codenvy.modeling.generator.maven.plugin;
 
-import com.codenvy.modeling.generator.GenerationController;
-import com.codenvy.modeling.generator.inject.GeneratorModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
+import com.codenvy.modeling.generator.GeneratorUtility;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -22,12 +19,9 @@ public class ModelGeneratorMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-
         MavenProject project = (MavenProject)getPluginContext().get("project");
         String baseDir = project.getBasedir().getPath();
 
-        Injector injector = Guice.createInjector(new GeneratorModule());
-        GenerationController generationController = injector.getInstance(GenerationController.class);
-        generationController.generate(baseDir);
+        GeneratorUtility.generate(baseDir);
     }
 }
